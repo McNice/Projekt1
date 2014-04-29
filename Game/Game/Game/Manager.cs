72d72cde875c537a1389c5466a45b49351640608
Dallor <simon.dahlgren@telia.com>
2014-04-29 16:11:37 +0200

    Level 1 Simon

--- Game/Game/Game/Manager.cs
+++ Game/Game/Game/Manager.cs
@@ -27,7 +27,7 @@ namespace Game
         public Manager()
         {
             map = new Map(Game1.TILESX, Game1.TILESY);
-            map.LoadMap("anton");
+            map.LoadMap("S1");
         }
 
         public void LoadContent()
