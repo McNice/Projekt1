3f14d598eeef64876668165d8caaf68b4fce4e9c
Bino87 <kallenved@gmail.com>
2014-04-29 15:23:41 +0200

    Ladder Class

---
+++ Game/Game/Game/Ladder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Game
+{
+    public class Ladder : Tile
+    {
+        public Ladder(Vector2 pos, string texName)
+            : base(pos, texName)
+        {
+            
+        }
+    }
+}
